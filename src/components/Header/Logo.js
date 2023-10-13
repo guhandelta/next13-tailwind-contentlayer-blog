3ab64ab756e28f8ece7a3fa14a60e6e58474cb15
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2023-10-13 09:28:02 -0400

    Minor fix

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import ProfileImg from '../../../project_files/profile-img.png'
+import ProfileImg from './profile-img.png'
 import Image from 'next/image'
 import Link from 'next/link'
 
